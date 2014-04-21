1ffad0ed55546aa356c13f6fe12713befea5fa73
zoe <zoechow8@gmail.com>
2014-04-21 19:21:44 +0800

    进阶版git 资源列表生成工具 -- 支持自定义目录

--- git_revision_xml_producer.sh
+++ git_revision_xml_producer.sh
@@ -6,91 +6,159 @@
 #	this script is a processor to generate a xml file that contains the whole resource list
 #   hash(git rev-parse format) of the git repository.
 # --------------------------------------------------
-# usage:
-#   git_revison_xml_processor.sh  gitweb@192.168.69.40:webgameJj_client v1.0 release /proj/git/client
+#	usage: git_revison_xml_producer.sh [-R <the git repository path>] [-T <the tag of the repository>] 
+#		   [-r <the relative root document that you want in the repository>]
+#           [-p <the target path>]
+#           [-h]
 #
-#   /proj/git/client : the location that you want clone (initilize) the repository to
+#	example: git_revison_xml_producer.sh -R /proj/git/client -T v1.0 -r release -p res,img,assets
 #
-#   v1.0 : the target repository tag that you want
-#
-#	release : the resource folder name in the repository
-#
-#	0 : mode (use for when exec by script not output the usage info)	
+#	Options:
+#		-R 		the git repository,There is must be have the \".git\" dir and use the absolute path.
+#		-T 		the tag of the repository in git.
+#		-r 		the relative root document,provide to you the list just relative to the root document.
+#		-p 		the target dir that you want,NOTE: it is relative to the repository root by default,if -r <relative root> is passed,relative to it.
+#		-h 		help info
 # --------------------------------------------------
+export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/bin"
+
+#help info
 usage(){
 mode="$1"
 [[ "${mode}" == "0" ]] && return
-echo -e "Usage: This script is use for get the files list with the file_name|hash (product from git rev-parse)\n
+echo "usage: git_revison_xml_producer.sh [-R <the git repository path>]
+                                       [-T <the tag of the repository>]
+                                       [-r <the relative root document that you want in the repository>]
+                                       [-p <the target path>] [-h]
 
-example: git_revison_xml_processor.sh v1.0 release /proj/git/client\n
+example: git_revison_xml_producer.sh -R /proj/git/client -T v1.0 -r release -p res,img,assets
 
-Options:\n
-  /proj/git/client                          the location that you want clone (initilize) the repository to,if not given any value,will set \$HOME as default\n
-  v1.0                                      the target repository tag that you want\n
-  release                                   the resource folder name in the repository\n"
+Options:
+	-R 		the git repository,There is must be have the \".git\" dir and use the absolute path.
+	-T 		the tag of the repository in git.
+	-r 		the relative root document,provide to you the list just relative to the root document.
+	-p 		the target dir that you want,NOTE: it is relative to the repository root by default,if -r <relative root> is passed,relative to it.
+	-m 		the mode: default 1,when eq 0, the help info will not output when exec error
+	-h 		help info
+"
+exit
 }
 
-export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/bin"
-local_repo_location="$1"
-git_tag="$2"
-resource_folder="$3"
-mode="$4"
+if [[ $# == 0 ]];then
+	usage
+fi
 
-if [[ "g$mode" == "g" ]];then
+while getopts R:T:r:p:m:h option 2>/dev/null
+do
+  case $option in
+    R)
+        local_repo_location=$OPTARG
+    ;;
+    T)
+        git_tag=$OPTARG
+    ;;
+    r)
+    	relative_root=$OPTARG
+   	;;
+   	p)
+   		resource_folder=$OPTARG
+   	;;
+   	m)
+   		mode=$OPTARG
+   	;;
+    h)
+        usage
+    ;;
+    \?)
+        Print_Msg "[\033[0;31;1mFailed\033[0m] ARGV-ERROR"
+    ;;
+  esac
+done
+
+if [[ "x$mode" == "x" ]];then
 mode=1
 fi
 
-if [ ! -d "$local_repo_location" ];then
+#check repo
+if [[ ! -d "$local_repo_location" ]];then
 	echo 'Error: local repo location not exists'
-	usage ${mode}
+	#usage ${mode}
 	exit 1
 fi
 
 if [[ ! -d ${local_repo_location%/}"/.git" ]];then
 	echo 'Error: the repository is not a git repository'
-	usage ${mode}
+	#usage ${mode}
 	exit 2
 fi
 
-cd ${local_repo_location}
 # check is the target repository is available.
+cd ${local_repo_location}
 git status &> /dev/null
 if [[ $? != 0 ]];then
 	echo 'Error: the repository is not available'
-	usage ${mode}
+	#usage ${mode}
 	exit 4
-fi 
-
-if [[ "g$git_tag" == "g" ]];then
-git_tag='master'
 fi
+
+# check git tag is exists
+if [[ "g$git_tag" == "g" ]]; then
+	git_tag='master'
+fi
+
 git checkout ${git_tag} &> /dev/null
 if [[ $? != 0 ]];then
-	echo -e "Error: checkout ${git_tag} fail"
-	usage ${mode}
+	echo "Error: checkout ${git_tag} fail"
+	#usage ${mode}
 	exit 4
 fi
 
-repo=${local_repo_location%/}/${resource_folder}
+# if passed relative folder 
+if [[ "r$relative_root" == "r" ]];then
+	repo=${local_repo_location%/}/
+else
+	repo=${local_repo_location%/}/${relative_root}
+fi
 
-if [[ "x$repo" == "x" || ! -d "${repo}" ]];then
- 	echo 'repo not exists'
- 	usage ${mode}
+if [[ ! -d "${repo}" ]];then
+ 	echo "repo ${repo} not exists"
+ 	#usage ${mode}
  	exit 3
 fi
 
+# if passed target dir
+if [[ "p$resource_folder" != "p" ]];then
+	resource_folder=($(echo "$resource_folder" | sed 's/,/ /g'))
+	cnt=0
+	for i in ${resource_folder[@]}
+	do
+		target_dir=${repo%/}/${i}
+		if [[ ! -d  ${target_dir} ]];then
+			echo "the target dir ${target_dir} not exists"
+			#usage ${mode}
+			exit 3
+		fi
+		if [[ $cnt == 0 ]];then
+			target_path="^./$i/"
+		else
+			target_path=${target_path}"|^./$i/"
+		fi
+		cnt=$(($cnt + 1))
+	done
+fi
+
 cd ${repo}
-IFS=$(echo -en "\n\b")
-echo -e '<xml version="1.0" charset="UTF-8">'
-echo -e '<Root GameVerRes="'${git_tag}'">';
-for file in `find . -type f | grep -Ev "/.git/"`
+#IFS=$(echo -en "\n\b")
+echo '<xml version="1.0" charset="UTF-8">'
+echo '<Root GameVerRes="'${git_tag}'">';
+find . -type f | sed 's/ /\\ /g' | grep -E "${target_path}" | grep -v "/.git/" | while read file
 do
 	hash=$(git rev-parse HEAD:"$file");
 	if [[ ! $(echo ${hash} | grep "HEAD:") ]];then
-		f=$(echo $file | sed 's/\.\///g');
-		size=$(du -b $file | awk '{print $1}')
-		echo -e '<Res file="'${f}'" ver="'${hash}'" size="'${size}'"/>'
+		f=$(echo "$file" | sed -e 's/\.\///g');
+		size=$(ls -lsa "$file" | awk '{print $(NF-4)}')
+		echo '<Res file="'${f}'" ver="'${hash}'" size="'${size}'"/>'
 	fi
 done
-echo -e "</Root>"
-echo -e "</xml>"
+echo  "</Root>"
+echo  "</xml>"
